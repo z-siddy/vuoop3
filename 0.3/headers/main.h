6776ad36fbebb4844769660f0f9efd5098e6fc3e
Žygimantas <zyg.sid2@gmail.com>
2019-02-27 10:00:01 +0200

    v0.3 added

---
+++ 0.3/headers/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <cmath>
+#include <iomanip>
+#include <random>
+#include <algorithm>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::setw;
+using std::left;
+using std::string;
+using std::setprecision;
+using std::fixed;
+using std::vector;
+using std::sort;
+
+#endif //MAIN_H
---
+++ 0.3/headers/studs.h
@@ -0,0 +1,20 @@
+#include "Student.h"
+
+#ifndef STUDS_H
+#define STUDS_H
+
+struct Studs{
+    vector<Student > students;
+    int longestName = 12;
+    int longestLastName = 12;
+    bool string_is_valid(const string x);
+    void readFileData(const char input[]);
+    void sortStudents();
+    void printResult();
+    void inputData();
+    void checkLongestNames();
+    bool checkFile(const char *filename);
+    bool isNumber(const std::string& s);
+};
+
+#endif //STUDS_H
